c90eec03b33f1f36331ee0eafc54ae24e5fdff73
David <ming.xie@uts.edu.au>
2022-02-11 05:53:35 +0000

    daily commit

---
+++ Docker/train_syn/benchmark_10.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function sync_bench() {
+    last_f=`ls -lt | awk '/_clustering/ {print $NF}'`
+    echo "$1${last_f}"
+    gsutil -m cp "./${last_f}" "gs://gs_bak/benchmark/$1${last_f}"
+    rm "./${last_f}"
+}
+
+
+cd /multi-center-fed-learning
+git pull origin rbmcfl
+cd /multi-center-fed-learning/models
+echo "****** Benchemark clustering to run on Synthetic ******"
+
+DATASET="SYNTHETIC"
+python experiment.py -dataset synthetic -experiment fedrobust_benchmark -configuration job-poison.yaml
+FILE="$DATASET-"
+sync_bench FILE
+
+echo "Training result copied to bucket, job successfully completed. "
---
+++ Docker/train_syn/training_alter.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+function sync_history() {
+    pushd ./metrics
+    last_f=`ls -lt | awk '/history/ {print $NF}'`
+    echo "$1${last_f}"
+    gsutil -m cp "./${last_f}" "gs://gs_bak/mcflhistory/$1${last_f}"
+    rm "./${last_f}"
+    popd
+}
+
+echo "****** Experiment to run on Synthetic ******"
+
+cd /multi-center-fed-learning
+git pull origin rbmcfl
+cd /multi-center-fed-learning/models
+DATASET="SYNTHETIC"
+python experiment.py -dataset synthetic -experiment fedavg
+FILE="$DATASET-fedavg-"
+sync_history "${FILE}"
+
+python experiment.py -dataset synthetic -experiment fedsgd
+FILE="$DATASET-fedsgd-"
+sync_history "${FILE}"
+echo "Result copied to storage, job successfully completed. "
