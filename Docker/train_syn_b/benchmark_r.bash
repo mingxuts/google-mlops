3202c222a3fbdf4eade9a2a12673d356b96bcd74
David <ming.xie@uts.edu.au>
2022-02-12 07:33:58 +0000

    container benchmark on celeba

---
+++ Docker/train_syn_b/benchmark_r.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+function sync_bench() {
+    last_f=`ls -lt | awk '/_clustering/ {print $NF}'`
+    echo "$1${last_f}"
+    gsutil -m cp "./${last_f}" "gs://gs_bak/benchmark/$1${last_f}"
+    rm "./${last_f}"
+}
+
+DATASET="synthetic"
+cd /multi-center-fed-learning
+git pull origin rbmcfl
+cd /multi-center-fed-learning/models
+echo "****** Benchemark clustering to run on ${DATASET} ******"
+
+python experiment.py -dataset "$DATASET" -experiment fedrobust_benchmark -configuration job-poison.yaml
+FILE="$DATASET-"
+sync_bench "${FILE}"
