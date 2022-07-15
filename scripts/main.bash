6b6e46e169e78e9e57a5e90f4eeb320f69af84d9
David Xie <davidxiem@gmail.com>
2022-07-15 00:30:20 +0000

    routine commit

--- scripts/main.bash
+++ scripts/main.bash
@@ -1,31 +1,27 @@
 #! /bin/bash
 
-DATASET="synthetic"
-lr="0.03"
+DATASET="femnist"
+lr="0.003"
 num_rounds="400"
-clients_per_round="3"
+clients_per_round="2"
 base_line="fedco"
 
 function run_fedco() {
-    rm ./output/*
     num_clusters="$1"
     python fl_main.py --dataset=${DATASET} --num_rounds=${num_rounds} --baseline=${base_line} --clients-per-round=${clients_per_round} --epochs=10 --test_batch=100 --batch-size=10 --lr=${lr} --eval_every=1 --k=${num_clusters} --seed_file=config/random_seed.csv --extra_params_file=../configs/fedrobust/job.yaml    
-    bq load --source_format=NEWLINE_DELIMITED_JSON explore.event ./output/event_data.json
-    bq load --source_format=NEWLINE_DELIMITED_JSON explore.test_raw ./output/test_data.json
-    bq load --source_format=NEWLINE_DELIMITED_JSON explore.ft_raw ./output/ft_data.json
 }
 
 ##################### Script #################################
-cd /root/leaf/data/synthetic
-rm -rf data
-python main.py -num-tasks 1000 -num-classes 5 -num-dim 60
-./preprocess.sh -s niid --sf 1.0 -k 5 -t sample --tf 0.6
+cd /root/leaf/data/femnist
+./preprocess.sh -s niid --sf 0.3 -k 30 -t sample --tf 0.8
 cd /root/multi-center-fed-learning
 git pull origin pfl-test
 cd models
 echo $(pwd)
 echo "Running Training Pipeline......."
-run_fedco 3
+run_fedco 2
 run_fedco 4
-run_fedco 5
+run_fedco 6
+
+gsutil -m cp ./output/* gs://gs_bakcentral/json/
 
